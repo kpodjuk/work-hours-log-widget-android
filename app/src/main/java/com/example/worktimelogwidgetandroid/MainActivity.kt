673ffaa9b0c2c92afc835af68edea05dda4ad2d0
kpodjuk <62662345+kpodjuk@users.noreply.github.com>
2023-08-14 19:48:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/worktimelogwidgetandroid/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.worktimelogwidgetandroid
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.app.ComponentActivity
+
+
+@SuppressLint("RestrictedApi")
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // serves layout from .xml to the user
+        setContentView(R.layout.main_activity)
+
+        // handle button press
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Log.i("MainActivity", "Button pressed!")
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "Wylosowano: ${(0..6).random()}"
+        }
+    }
+
+
+}
