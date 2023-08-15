d7e8381be3e9abdd3396d5ff97164f882c44ade2
kpodjuk <62662345+kpodjuk@users.noreply.github.com>
2023-08-15 16:03:48 +0200

    font resource added
    fdsafdsa

--- app/src/main/java/com/example/worktimelogwidgetandroid/MainActivity.kt
+++ app/src/main/java/com/example/worktimelogwidgetandroid/MainActivity.kt
@@ -4,7 +4,17 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.TextView
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ComponentActivity
 
 
@@ -28,6 +38,20 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @Composable
+    fun Button(
+        onClick: () -> Unit,
+        modifier: Modifier = Modifier,
+        enabled: Boolean = true,
+        shape: androidx.compose.ui.graphics.Shape = ButtonDefaults.shape,
+        colors: ButtonColors = ButtonDefaults.buttonColors(),
+        elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+        border: BorderStroke? = null,
+        contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+        interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+        content: @Composable RowScope.() -> Unit
+    ): Unit {
+    }
 
 }
 
